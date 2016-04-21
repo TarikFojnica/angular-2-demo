88f6907ce2c9284ff1d473d12da61a8c6d7144c7
TarikFojnica <tarik.f786@gmail.com>
2016-04-21 10:19:23 +0200

    initial commit

---
+++ app/app.component.js
@@ -0,0 +1,62 @@
+System.register(['angular2/core'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var Hero, AppComponent, HEROES;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            Hero = (function () {
+                function Hero() {
+                }
+                Hero = __decorate([
+                    core_1.Component({
+                        selector: 'my-app',
+                        template: "\n    <h2>{{selectedHero.name}} details!</h2>\n    <div><label>id: </label>{{selectedHero.id}}</div>\n    <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n    </div>\n\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n        <li *ngFor=\"#hero of heroes\" (click)=\"onSelect(hero)\">\n            <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n        </li>\n    </ul>\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], Hero);
+                return Hero;
+            })();
+            exports_1("Hero", Hero);
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.title = 'Tarik Infolog 2';
+                    this.selectedHero = {
+                        id: 1,
+                        name: 'Tarik Fojnica 2'
+                    };
+                    this.heroes = HEROES;
+                }
+                AppComponent.prototype.onSelect = function (hero) {
+                    this.selectedHero = hero;
+                };
+                return AppComponent;
+            })();
+            exports_1("AppComponent", AppComponent);
+            HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ];
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
